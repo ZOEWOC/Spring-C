c88fc428b4f7c666911ad2e5fbd81ba4dab3cdf2
zoewoc <zoewoc@outlook.com>
2020-12-17 23:37:34 +0800

    设值注入

---
+++ ch02-di-xml/src/main/java/com/zoewoc/ba02/School.java
@@ -0,0 +1,17 @@
+package com.zoewoc.ba02;
+
+public class School {
+    private String name;
+    private String address;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    
+}
+
